d2ff3d5ad899276660dd13c1e9c00a0c4fb08658
Yihan <yihan.wang.1@stonybrook.edu>
2021-06-03 12:57:25 -0400

    add single flyby

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 CXX=g++
 CXXFLAGS=-Wall -std=c++17 -O3 -pthread
 
-all:flyby
+all:flyby flyby-single
 
 flyby: 
 	$(CXX) $(CXXFLAGS) -o flyby scattering.cpp
+
+flyby-single: 
+	$(CXX) $(CXXFLAGS) -o flyby-single scattering-single.cpp
 
 clean:
 	rm -f flyby
--- scattering-single.cpp
+++ scattering-single.cpp
@@ -24,10 +24,10 @@ void job(size_t thread_id, size_t scattering_num, double b_semi_major_axis) {
 
     print(post_flyby_file,
           "m0+mj1+mj2 [solar mass], m1 [solar mass], p_hyper [au], e_hyper, inc_hyper [arc], Omega_hyper [arc], "
+          "omega_hyper [arc], nv_hyper [arc], "
           "m0 [solar mass], mj1 [solar mass], a_j1 [au], e_j1, inc_j1 [arc], Omega_j1 [arc], omega_j1 [arc], "
           "nv_j1 [arc], m0+mj1 [solar mass], mj2 [solar mass], a_j2 [au], e_j2, inc_j2 [arc], Omega_j2 [arc], "
-          "omega_j2 [arc], nv_j2 [arc], m1 [solar mass], m2 [solar mass], a_b [au], e_b, inc_b [arc], Omega_b "
-          "[arc], omega_b [arc], nv_b [arc]\n post-flyby particles data [time, id, mass, x, y, z, vx, vy, "
+          "omega_j2 [arc], nv_j2 [arc]\n post-flyby particles data [time, id, mass, x, y, z, vx, vy, "
           "vz](per line)\n");
 
     print(post_flyby_file, std::setprecision(16));
@@ -38,8 +38,7 @@ void job(size_t thread_id, size_t scattering_num, double b_semi_major_axis) {
         Particle star{1_Ms};
         Particle jupiter1{1_Mj};
         Particle jupiter2{1_Mj};
-        Particle star1{1_Ms};
-        Particle star2{1_Ms};
+        Particle star1{2_Ms};
 
         // create planetary system with two giant planets
         double planet_inc = random::Uniform(0, consts::pi);
@@ -55,35 +54,28 @@ void job(size_t thread_id, size_t scattering_num, double b_semi_major_axis) {
 
         move_particles(jupiter2_orb, jupiter2);
 
-        // create binary star
-        auto bin_orb = Elliptic(star1.mass, star2.mass, b_semi_major_axis, 0.0, isotherm, isotherm, isotherm, isotherm);
-
-        move_particles(bin_orb, star2);
-
-        move_to_COM_frame(star1, star2);
-
         // create scattering hyperbolic orbit
 
-        double b_max = calc_max_impact_parameter(a_j2 * 2, v_inf, M_tot(star, jupiter1, jupiter2, star1, star2)) +
-                       b_semi_major_axis / 2;
+        double b_max =
+            calc_max_impact_parameter(a_j2 * 2, v_inf, M_tot(star, jupiter1, jupiter2, star1)) + b_semi_major_axis / 2;
 
         auto incident_orb =
-            scattering::incident_orbit(M_tot(star, jupiter1, jupiter2), M_tot(star1, star2), v_inf, b_max, r_start);
+            scattering::incident_orbit(M_tot(star, jupiter1, jupiter2), star1.mass, v_inf, b_max, r_start);
 
-        move_particles(incident_orb, star1, star2);
+        move_particles(incident_orb, star1);
 
-        move_to_COM_frame(star, jupiter1, jupiter2, star1, star2);
+        move_to_COM_frame(star, jupiter1, jupiter2, star1);
 
-        double scattering_t_end = 2 * time_to_periapsis(group(star, jupiter1, jupiter2), group(star1, star2));
+        double scattering_t_end = 2 * time_to_periapsis(group(star, jupiter1, jupiter2), star1);
 
-        Solver sim{0, star, jupiter1, jupiter2, star1, star2};
+        Solver sim{0, star, jupiter1, jupiter2, star1};
 
         Solver::RunArgs args;
 
         args.add_stop_condition(scattering_t_end);
 
         args.add_stop_point_operation([&](auto& ptc, auto h) {
-            print(post_flyby_file, incident_orb, ',', jupiter1_orb, ',', jupiter2_orb, ',', bin_orb, '\n', ptc, '\n');
+            print(post_flyby_file, incident_orb, ',', jupiter1_orb, ',', jupiter2_orb, '\n', ptc, '\n');
         });
 
         sim.run(args);
